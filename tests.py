83672919f7ef7dcdb65c93a4ffabb82135045bd7
alicewuhhh <71121323+alicewuhhh@users.noreply.github.com>
2022-09-06 13:51:54 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -9,10 +9,3 @@ def test_it():
 	s = Stack()
 	s.push(1)
  	assert s.size() ==1
-	
-# 	assert s.size == 0
-
-# 	for i in range(1):
-#   		s.push(i)
-# 	assert s.size() = 101
-# 	raise NotImplementedError('Need to implement a simple test.')
